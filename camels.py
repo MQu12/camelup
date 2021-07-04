49b098e3b545ea25e0fe3dd021f5f5c9f4f7c377
Tom <thomas.powell263@gmail.com>
2021-07-04 17:41:39 +0100

    Rejigged camel system

--- camels.py
+++ camels.py
@@ -7,9 +7,10 @@ Created on Sun Jul  4 16:19:19 2021
 
 class camel:
     
-    def __init__(self,id,position):
+    def __init__(self,id,position=0,stack_position=0):
         self.id = id
         self.position = position
+        self.stack_position = stack_position
         
     def camel_type(self):
         raise NotImplementedError
--- camelup.py
+++ camelup.py
@@ -5,69 +5,29 @@ Created on Sun Jul  4 15:55:25 2021
 @author: Dr. P
 """
 
-import numpy as np
 from camels import racing_camel, crazy_camel
 
-number_of_racing_camels = 5
-number_of_crazy_camels = 2
-number_of_tiles = 16
+track_length = 16
 
+#create camels
+camel_list = []
 
-def init_track(n_tiles):
-    '''
-    Generate the track
+camel_list.append( racing_camel(0,0) )
+camel_list.append( racing_camel(1,0) )
+camel_list.append( racing_camel(2,1) )
+camel_list.append( racing_camel(3,2) )
+camel_list.append( racing_camel(4,2) )
+camel_list.append( crazy_camel(0,15) )
+camel_list.append( crazy_camel(1,14) )
+
+def set_stack(camel_list):
     
-    Args:
-        n_tiles (int): number of tiles
+    positions = []
+    
+    for i in range(len(camel_list)):
         
-    Returns:
-        track (list[list]): the track
-    '''
-    track = []
-    
-    for i in range(n_tiles):
-        track.append([])
-        
-    return track
+        camels_on_tile = positions.count(camel_list[i].position)
+        camel_list[i].stack_position = camels_on_tile
+        positions.append(camel_list[i].position)
 
-def generate_camels(n_racing,n_crazy):
-    '''
-    Generate the camels
-    
-    Parameters
-    ----------
-    n_racing : int
-        Number of racing camels to generate
-    n_crazy : int
-        Number of crazy camels to generate
-
-    Returns
-    -------
-    racing_camels : list[racing_camel]
-        List of racing camels.
-    crazy_camels : list[crazy_camels]
-        List of crazy camels.
-
-    '''
-    
-    racing_camels = []
-    crazy_camels = []
-    
-    for i in range(n_racing):
-        racing_camels.append(racing_camel(i))
-    
-    for i in range(n_crazy):
-        crazy_camels.append(crazy_camel(i))
-        
-    return racing_camels, crazy_camels
-    
-track = init_track(number_of_tiles)
-racing_camels, crazy_camels = generate_camels(number_of_racing_camels,number_of_crazy_camels)
-
-# Add camels to track
-track[0].append(racing_camels[0])
-track[0].append(racing_camels[1])
-track[1].append(racing_camels[2])
-track[2].append(racing_camels[3])
-track[2].append(racing_camels[4])
-
+set_stack(camel_list)        
