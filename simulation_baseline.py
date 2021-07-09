eb2bf0f893ba3832cbd5cf89553c099e1d35fc37
Tom <thomas.powell263@gmail.com>
2021-07-09 21:57:48 +0100

    added regression tests

--- simulation_baseline.py
+++ simulation_baseline.py
@@ -2,6 +2,8 @@
 """
 Created on Wed Jul  7 21:46:32 2021
 
+Script to check updated results against baselines
+
 @author: Dr. P
 """
 
@@ -9,21 +11,70 @@ from camelup import race_state
 from camels import racing_camel, crazy_camel
 import monte_carlo
 import make_plots
-
-camel_list = []
-
-camel_list.append( racing_camel(0,0) )
-camel_list.append( racing_camel(1,0) )
-camel_list.append( racing_camel(2,1) )
-camel_list.append( racing_camel(3,2) )
-camel_list.append( racing_camel(4,2) )
-camel_list.append( crazy_camel(0,15) )
-camel_list.append( crazy_camel(1,14) )
-
-state = race_state(camel_list)
+import os.path
+import numpy as np
+import random
 
 n=1000
-leg_winners = monte_carlo.simulate_leg_n_times(state,n)
-make_plots.plot_win_probs(leg_winners,'Leg win probability')
-race_winners = monte_carlo.simulate_n_races(state,n)
-make_plots.plot_win_probs(race_winners,'Race win probability')
+
+def init_race():
+    camel_list = []
+    
+    camel_list.append( racing_camel(0,0) )
+    camel_list.append( racing_camel(1,0) )
+    camel_list.append( racing_camel(2,1) )
+    camel_list.append( racing_camel(3,2) )
+    camel_list.append( racing_camel(4,2) )
+    camel_list.append( crazy_camel(0,15) )
+    camel_list.append( crazy_camel(1,14) )
+    
+    state = race_state(camel_list)
+
+    return state
+
+def run_leg_baseline(state,plot=False):
+    random.seed(1)
+    leg_winners = monte_carlo.simulate_leg_n_times(state,n)
+    if plot:
+        make_plots.plot_win_probs(leg_winners,'Leg win probability')
+    return leg_winners
+
+def run_race_baseline(state,plot=False):
+    random.seed(1)
+    race_winners = monte_carlo.simulate_n_races(state,n)
+    if plot:
+        make_plots.plot_win_probs(race_winners,'Race win probability')
+    return race_winners
+    
+def create_baselines():
+    
+    state = init_race()
+    
+    folder = 'baselines/'
+    leg1000_name = 'leg1000_baseline.npy'
+    race1000_name = 'race1000_baseline.npy'
+    
+    if not os.path.isfile(folder+leg1000_name):
+        leg1000_array = run_leg_baseline(state)
+        with open(folder+leg1000_name, 'wb') as f:
+            np.save(f, leg1000_array)
+        print(f'Created new baseline {folder+leg1000_name}')
+    else:
+        print(f'Baseline {folder+leg1000_name} already exists, skipping')
+    
+    if not os.path.isfile(folder+race1000_name):
+        race1000_array = run_race_baseline(state)
+        with open(folder+race1000_name, 'wb') as f:
+            np.save(f, race1000_array)
+        print(f'Created new baseline {folder+race1000_name}')
+    else:
+        print(f'Baseline {folder+race1000_name} already exists, skipping')
+    
+
+def main():
+    state = init_race()
+    run_leg_baseline(state,True)
+    run_race_baseline(state,True)
+    
+if __name__ == "__main__":
+    main()
---
+++ test_regression_tests.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 21:41:58 2021
+
+@author: Dr. P
+"""
+
+import numpy as np
+import simulation_baseline
+import random
+
+random.seed(1)
+
+folder = 'baselines/'
+
+def test_vs_leg_baseline():
+    
+    leg1000_name = 'leg1000_baseline.npy'
+    
+    with open(folder+leg1000_name, 'rb') as f:
+        baseline = np.load(f)
+        
+    state = simulation_baseline.init_race()
+    
+    result = simulation_baseline.run_leg_baseline(state)
+
+    np.testing.assert_array_equal(baseline,result)
+    
+def test_vs_race_baseline():
+    
+    race1000_name = 'race1000_baseline.npy'
+    
+    with open(folder+race1000_name, 'rb') as f:
+        baseline = np.load(f)
+        
+    state = simulation_baseline.init_race()
+    
+    result = simulation_baseline.run_race_baseline(state)
+
+    np.testing.assert_array_equal(baseline,result)
+    
