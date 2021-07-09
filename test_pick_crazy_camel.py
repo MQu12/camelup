9d38a1d89df9a751a4ef416e888641ce03fb9748
Tom <thomas.powell263@gmail.com>
2021-07-09 21:26:35 +0100

    fixed tests

--- test_pick_crazy_camel.py
+++ test_pick_crazy_camel.py
@@ -5,7 +5,7 @@ Created on Tue Jul  6 22:11:28 2021
 @author: Dr. P
 """
 
-from camelup import pick_crazy_camel
+from camelup import race_state
 from camels import racing_camel, crazy_camel
 import random
 
@@ -16,8 +16,10 @@ def test_random_camel_is_picked_when_crazy_camels_are_apart(mocker):
     camel_list = []
     camel_list.append( crazy_camel(0,5) )
     camel_list.append( crazy_camel(1,6) )
+    
+    state = race_state(camel_list)
 
-    pick_crazy_camel(camel_list,racing_camel_count=0)
+    state.pick_crazy_camel()
     
     random.randint.assert_called_once()
 
@@ -28,10 +30,10 @@ def test_top_camel_is_picked_when_crazy_camels_are_stacked(mocker):
     camel_list = []
     camel_list.append( crazy_camel(0,5) )
     camel_list.append( crazy_camel(1,5) )
-    
-    camel_list[1].stack_position = 1
 
-    crazy_camel_no = pick_crazy_camel(camel_list,racing_camel_count=0)
+    state = race_state(camel_list)
+
+    crazy_camel_no = state.pick_crazy_camel()
     
     random.randint.assert_not_called()
     
@@ -42,14 +44,13 @@ def test_camel_carrying_racing_camels_is_picked(mocker):
     mocker.patch('random.randint')
     
     camel_list = []
-    camel_list.append( racing_camel(0,5) )
     camel_list.append( crazy_camel(0,9) )
     camel_list.append( crazy_camel(1,5) )
-    
-    
-    camel_list[0].stack_position = 1
+    camel_list.append( racing_camel(0,5) )
 
-    crazy_camel_no = pick_crazy_camel(camel_list,racing_camel_count=1)
+    state = race_state(camel_list)
+
+    crazy_camel_no = state.pick_crazy_camel()
     
     random.randint.assert_not_called()
     
@@ -64,12 +65,10 @@ def test_camel_picked_at_random_when_both_are_carrying_crazy_camels(mocker):
     camel_list.append( racing_camel(1,9) )
     camel_list.append( crazy_camel(0,9) )
     camel_list.append( crazy_camel(1,5) )
-    
-    
-    camel_list[0].stack_position = 1
-    camel_list[1].stack_position = 1
 
-    pick_crazy_camel(camel_list,racing_camel_count=2)
+    state = race_state(camel_list)
+
+    state.pick_crazy_camel()
     
     random.randint.assert_called_once()
     
@@ -82,11 +81,11 @@ def test_camel_at_bottom_of_stack_is_picked_when_there_is_a_racing_camel_between
     camel_list.append( crazy_camel(0,5) )
     camel_list.append( crazy_camel(1,5) )
     
+    state = race_state(camel_list)
+    state.camel_list[0].stack_position = 1
+    state.camel_list[1].stack_position = 0
     
-    camel_list[0].stack_position = 1
-    camel_list[2].stack_position = 2
-
-    crazy_camel_no = pick_crazy_camel(camel_list,racing_camel_count=1)
+    crazy_camel_no = state.pick_crazy_camel()
     
     random.randint.assert_not_called()
     
