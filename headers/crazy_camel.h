1a7cdf0b9d2e0d380b5b6f54fa80d70006edc13f
Tom <thomas.powell263@gmail.com>
2021-07-24 19:51:13 +0100

    added crazy camels

---
+++ headers/crazy_camel.h
@@ -0,0 +1,19 @@
+#ifndef CRAZY_CAMEL
+#define CRAZY_CAMEL
+
+#include "camel.h"
+
+struct crazy_camel: public camel{
+
+public:
+	crazy_camel(int _id, int _position, int _stack_position=0, int _total_movement=0):camel(_id,_position,_stack_position,_total_movement){
+
+	}
+	~crazy_camel(){
+		
+	}
+	int direction();
+
+};
+
+#endif
---
+++ src/crazy_camel.cpp
@@ -0,0 +1,5 @@
+#include "crazy_camel.h"
+
+int crazy_camel::direction(){
+	return -1;
+}
